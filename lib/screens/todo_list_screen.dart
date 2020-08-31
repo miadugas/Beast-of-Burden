627bf032ef853f5f146ab9228d3ec85eb0d6c0c4
Mia Dugas <miadugas@outlook.com>
2020-08-31 12:19:49 -0600

    main screen factored

--- lib/screens/todo_list_screen.dart
+++ lib/screens/todo_list_screen.dart
@@ -6,6 +6,28 @@ class TodoListScreen extends StatefulWidget {
 }
 
 class _TodoListScreenState extends State<TodoListScreen> {
+  Widget _buildTask(int index) {
+return Padding(
+    padding: EdgeInsets.symmetric(horizontal: 25.0),
+    child: Column(
+      children: <Widget>[
+        ListTile(
+          title: Text('Text Title'),
+          subtitle: Text('Aug 31, 2020 • High'),
+          trailing: Checkbox(
+            onChanged: (value) {
+              print(value);
+            },
+            activeColor: Theme.of(context).primaryColor,
+            value: true,
+          ),
+        ),
+        Divider(),
+      ],
+    ),
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -16,31 +38,40 @@ class _TodoListScreenState extends State<TodoListScreen> {
       ),
       body: ListView.builder(
         padding: EdgeInsets.symmetric(vertical: 80.0),
-        itemCount: 1,
+        itemCount: 10,
         itemBuilder: (BuildContext context, int index) {
           if (index == 0) {
-            return Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: <Widget>[
-                Text('My Tasks', style: TextStyle(color: Colors.black, 
-                fontSize: 40.0,
-                fontWeight: FontWeight.bold,
-                ),
-                ),
-                SizedBox(height: 10.0,),
-              Text(
-                '1 of 10',
-                style: TextStyle(
-                  color: Colors.grey,
-                  fontSize: 20.0,
-                  fontWeight: FontWeight.w600,
-                ),
-                ),
-              ],
+            return Padding(
+              padding: EdgeInsets.symmetric(horizontal: 40.0, vertical: 20.0),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: <Widget>[
+                  Text(
+                    'My Tasks',
+                    style: TextStyle(
+                      color: Colors.black,
+                      fontSize: 40.0,
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
+                  SizedBox(
+                    height: 10.0,
+                  ),
+                  Text(
+                    '1 of 10',
+                    style: TextStyle(
+                      color: Colors.grey,
+                      fontSize: 20.0,
+                      fontWeight: FontWeight.w600,
+                    ),
+                  ),
+                ],
+              ),
             );
           }
+          return _buildTask(index);
         },
-    ),
+      ),
     );
   }
 }
